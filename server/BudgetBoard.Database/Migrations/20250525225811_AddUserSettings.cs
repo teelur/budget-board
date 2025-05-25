66fd23f1e5d272ebe18bec50ca8ad0bb1e83ff6a
tshea <Tylershea12@gmail.com>
2025-05-25 18:02:31 -0500

    Add currency option

---
+++ server/BudgetBoard.Database/Migrations/20250525225811_AddUserSettings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BudgetBoard.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserSettings",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uuid", nullable: false),
+                    Currency = table.Column<char>(type: "character(1)", nullable: false),
+                    UserID = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserSettings", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_UserSettings_User_UserID",
+                        column: x => x.UserID,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserSettings_UserID",
+                table: "UserSettings",
+                column: "UserID",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserSettings");
+        }
+    }
+}
---
+++ server/BudgetBoard.Database/Models/UserSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace BudgetBoard.Database.Models;
+
+public class UserSettings()
+{
+    public Guid ID { get; set; } = Guid.NewGuid();
+    public char Currency { get; set; } = '$';
+    public Guid UserID { get; set; } = Guid.Empty;
+    public ApplicationUser User { get; set; } = new();
+}
