5076a928c904864bf0d4b13f0d485dec2d4870c1
tshea <Tylershea12@gmail.com>
2025-08-21 21:38:38 -0500

    Add accounts

--- client/src/app/Authorized/PageContent/Goals/GoalDetails/GoalDetails.tsx
+++ client/src/app/Authorized/PageContent/Goals/GoalDetails/GoalDetails.tsx
@@ -1,6 +1,8 @@
-import { Drawer, Skeleton, Text } from "@mantine/core";
+import { Card, Drawer, Skeleton, Stack, Text } from "@mantine/core";
 import { IGoalResponse } from "~/models/goal";
 import React from "react";
+import AccountItem from "~/components/AccountItem/AccountItem";
+import { IAccount } from "~/models/account";
 
 interface GoalDetailsProps {
   goal: IGoalResponse | null;
@@ -24,7 +26,14 @@ const GoalDetails = (props: GoalDetailsProps): React.ReactNode => {
       {props.goal === null ? (
         <Skeleton height={425} radius="lg" />
       ) : (
-        <Text>Test</Text>
+        <Stack gap="0.5rem">
+          <Text fw={600}>Accounts</Text>
+          {props.goal.accounts.map((account: IAccount) => (
+            <Card key={account.id} radius="md">
+              <AccountItem account={account} />
+            </Card>
+          ))}
+        </Stack>
       )}
     </Drawer>
   );
