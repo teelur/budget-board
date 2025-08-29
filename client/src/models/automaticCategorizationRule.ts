3de74929a8d02be6b667e78e8ccea5a41d018904
tshea <Tylershea12@gmail.com>
2025-08-28 22:17:51 -0500

    update types

--- client/src/models/automaticCategorizationRule.ts
+++ client/src/models/automaticCategorizationRule.ts
@@ -1,10 +1,39 @@
+export interface IRuleParameterCreateRequest {
+  field: string;
+  operator: string;
+  value: string;
+  type: string;
+}
+
 export interface IAutomaticCategorizationRuleRequest {
-  categorizationRule: string;
-  category: string;
+  conditions: IRuleParameterCreateRequest[];
+  actions: IRuleParameterCreateRequest[];
+}
+
+export interface IRuleParameterResponse {
+  id: string;
+  field: string;
+  operator: string;
+  value: string;
+  type: string;
 }
 
 export interface IAutomaticCategorizationRuleResponse {
   id: string;
-  categorizationRule: string;
-  category: string;
+  conditions: IRuleParameterResponse[];
+  actions: IRuleParameterResponse[];
+}
+
+export interface IRuleParameterUpdateRequest {
+  id: string;
+  field: string;
+  operator: string;
+  value: string;
+  type: string;
+}
+
+export interface IAutomaticCategorizationRuleUpdateRequest {
+  id: string;
+  conditions: IRuleParameterUpdateRequest[];
+  actions: IRuleParameterUpdateRequest[];
 }
