4759f7118aae2e3034d3c2e1a2a6c8aac1d40a33
tshea <Tylershea12@gmail.com>
2025-09-03 22:20:58 -0500

    update list to use new delete

--- client/src/app/Authorized/PageContent/Transactions/TransactionsHeader/TransactionsSettings/AutomaticRules/AutomaticRuleCard/ActionItem/ActionItem.tsx
+++ client/src/app/Authorized/PageContent/Transactions/TransactionsHeader/TransactionsSettings/AutomaticRules/AutomaticRuleCard/ActionItem/ActionItem.tsx
@@ -1,6 +1,7 @@
 import { Card, Group, Text } from "@mantine/core";
 import { getFormattedValue } from "~/helpers/automaticRules";
 import {
+  ActionOperators,
   IRuleParameterResponse,
   TransactionFields,
 } from "~/models/automaticRule";
@@ -13,6 +14,38 @@ interface ActionItemProps {
 }
 
 const ActionItem = (props: ActionItemProps) => {
+  const getCardContent = (): React.ReactNode => {
+    if (props.action.operator === "delete") {
+      return (
+        <Text fw={600} size="sm">
+          the transaction
+        </Text>
+      );
+    } else if (props.action.operator === "set") {
+      return (
+        <>
+          <Text fw={600} size="sm">
+            {TransactionFields.find(
+              (field) => field.value === props.action.field
+            )?.label ?? props.action.field}
+          </Text>
+          <Text fw={600} size="sm">
+            to
+          </Text>
+          <Text fw={600} size="sm">
+            {getFormattedValue(
+              props.action.field,
+              props.action.value,
+              props.currency,
+              props.categories
+            )}
+          </Text>
+        </>
+      );
+    }
+    return null;
+  };
+
   return (
     <Card
       bg="var(--mantine-color-card-alternate)"
@@ -22,23 +55,10 @@ const ActionItem = (props: ActionItemProps) => {
     >
       <Group gap="0.3rem">
         <Text fw={600} size="sm">
-          Set
+          {ActionOperators.find((op) => op.value === props.action.operator)
+            ?.label ?? props.action.operator}
         </Text>
-        <Text fw={600} size="sm">
-          {TransactionFields.find((field) => field.value === props.action.field)
-            ?.label ?? props.action.field}
-        </Text>
-        <Text fw={600} size="sm">
-          to
-        </Text>
-        <Text fw={600} size="sm">
-          {getFormattedValue(
-            props.action.field,
-            props.action.value,
-            props.currency,
-            props.categories
-          )}
-        </Text>
+        {getCardContent()}
       </Group>
     </Card>
   );
