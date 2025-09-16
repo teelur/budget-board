5af551acb7d149fa9554ac8512f652f6c96d1b83
tshea <Tylershea12@gmail.com>
2025-09-15 22:49:54 -0500

    Change filter card look

--- client/src/app/Authorized/PageContent/Transactions/TransactionsHeader/FilterCard/FilterCard.tsx
+++ client/src/app/Authorized/PageContent/Transactions/TransactionsHeader/FilterCard/FilterCard.tsx
@@ -2,7 +2,7 @@ import CategorySelect from "~/components/CategorySelect";
 import classes from "./FilterCard.module.css";
 
 import AccountSelectInput from "~/components/AccountSelectInput";
-import { Card, Flex, Stack, Title } from "@mantine/core";
+import { Flex, Stack, Text, Button } from "@mantine/core";
 import { DatePickerInput, DatesRangeValue } from "@mantine/dates";
 import { Filters } from "~/models/transaction";
 import React from "react";
@@ -22,54 +22,66 @@ const FilterCard = (props: FilterCardProps): React.ReactNode => {
   }
 
   return (
-    <Card className={classes.root} radius="md">
-      <Stack gap="0.5rem">
-        <Title order={3}>Filters</Title>
-        <Flex
-          className={classes.group}
-          direction={{ base: "column", sm: "row" }}
-          wrap="nowrap"
-          gap="md"
+    <Stack gap="0.5rem">
+      <Text fw={600} size="md">
+        Filters
+      </Text>
+      <Flex
+        className={classes.group}
+        direction={{ base: "column", sm: "row" }}
+        wrap="nowrap"
+        gap="md"
+      >
+        <DatePickerInput
+          w={{ base: "100%", sm: "25%" }}
+          type="range"
+          placeholder="Pick a date range"
+          value={props.filters.dateRange}
+          onChange={(dateRange: DatesRangeValue<string>) => {
+            const parsedDateRange: [Date | null, Date | null] = [
+              dateRange[0] ? dayjs(dateRange[0]).toDate() : null,
+              dateRange[1] ? dayjs(dateRange[1]).toDate() : null,
+            ];
+            props.setFilters({
+              ...props.filters,
+              dateRange: parsedDateRange,
+            });
+          }}
+        />
+        <AccountSelectInput
+          w={{ base: "100%", sm: "50%" }}
+          selectedAccountIds={props.filters.accounts}
+          setSelectedAccountIds={(newAccountIds: string[]) => {
+            props.setFilters({
+              ...props.filters,
+              accounts: newAccountIds,
+            });
+          }}
+          hideHidden
+        />
+        <CategorySelect
+          w={{ base: "100%", sm: "20%" }}
+          categories={props.categories}
+          value={props.filters.category}
+          onChange={(val) =>
+            props.setFilters({ ...props.filters, category: val })
+          }
+          withinPortal
+        />
+        <Button
+          w={{ base: "100%", sm: "130px" }}
+          onClick={() => {
+            props.setFilters({
+              dateRange: [null, null],
+              accounts: [],
+              category: "",
+            });
+          }}
         >
-          <DatePickerInput
-            w={{ base: "100%", sm: "30%" }}
-            type="range"
-            placeholder="Pick a date range"
-            value={props.filters.dateRange}
-            onChange={(dateRange: DatesRangeValue<string>) => {
-              const parsedDateRange: [Date | null, Date | null] = [
-                dateRange[0] ? dayjs(dateRange[0]).toDate() : null,
-                dateRange[1] ? dayjs(dateRange[1]).toDate() : null,
-              ];
-              props.setFilters({
-                ...props.filters,
-                dateRange: parsedDateRange,
-              });
-            }}
-          />
-          <AccountSelectInput
-            w={{ base: "100%", sm: "50%" }}
-            selectedAccountIds={props.filters.accounts}
-            setSelectedAccountIds={(newAccountIds: string[]) => {
-              props.setFilters({
-                ...props.filters,
-                accounts: newAccountIds,
-              });
-            }}
-            hideHidden
-          />
-          <CategorySelect
-            w={{ base: "100%", sm: "20%" }}
-            categories={props.categories}
-            value={props.filters.category}
-            onChange={(val) =>
-              props.setFilters({ ...props.filters, category: val })
-            }
-            withinPortal
-          />
-        </Flex>
-      </Stack>
-    </Card>
+          Clear Filters
+        </Button>
+      </Flex>
+    </Stack>
   );
 };
 
