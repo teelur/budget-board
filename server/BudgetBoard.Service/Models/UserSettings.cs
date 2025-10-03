6a5ec09f72bdf3e9bd00d0ce6b9363805150f054
tshea <Tylershea12@gmail.com>
2025-10-02 20:44:45 -0500

    add new setting

--- server/BudgetBoard.Service/Models/UserSettings.cs
+++ server/BudgetBoard.Service/Models/UserSettings.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Runtime.InteropServices.Marshalling;
+using System.Text.Json.Serialization;
 using BudgetBoard.Database.Models;
 
 namespace BudgetBoard.Service.Models;
@@ -7,24 +8,28 @@ public interface IUserSettingsResponse
 {
     string Currency { get; }
     int BudgetWarningThreshold { get; }
+    int ForceSyncLookbackMonths { get; }
 }
 
 public class UserSettingsResponse : IUserSettingsResponse
 {
     public string Currency { get; set; }
     public int BudgetWarningThreshold { get; set; }
+    public int ForceSyncLookbackMonths { get; set; }
 
     [JsonConstructor]
     public UserSettingsResponse()
     {
         Currency = "USD";
         BudgetWarningThreshold = 80;
+        ForceSyncLookbackMonths = 0;
     }
 
     public UserSettingsResponse(UserSettings userSettings)
     {
         Currency = userSettings.Currency.ToString();
         BudgetWarningThreshold = userSettings.BudgetWarningThreshold;
+        ForceSyncLookbackMonths = userSettings.ForceSyncLookbackMonths;
     }
 }
 
@@ -32,17 +37,20 @@ public interface IUserSettingsUpdateRequest
 {
     public string? Currency { get; }
     public int? BudgetWarningThreshold { get; }
+    public int? ForceSyncLookbackMonths { get; }
 }
 
 public class UserSettingsUpdateRequest : IUserSettingsUpdateRequest
 {
     public string? Currency { get; set; }
     public int? BudgetWarningThreshold { get; set; }
+    public int? ForceSyncLookbackMonths { get; set; }
 
     [JsonConstructor]
     public UserSettingsUpdateRequest()
     {
         Currency = null;
         BudgetWarningThreshold = null;
+        ForceSyncLookbackMonths = null;
     }
 }
