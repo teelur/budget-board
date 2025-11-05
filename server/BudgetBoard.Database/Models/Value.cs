53f4947121ee3f889e7826b425cfb630408e5bc5
teelur <tylershea12@gmail.com>
2025-11-05 14:27:48 -0500

    setup value service

--- server/BudgetBoard.Database/Models/Value.cs
+++ server/BudgetBoard.Database/Models/Value.cs
@@ -5,6 +5,7 @@ public class Value
     public Guid ID { get; set; }
     public decimal Amount { get; set; }
     public DateTime DateTime { get; set; }
+    public DateTime? Deleted { get; set; } = null;
     public required Guid AssetID { get; set; }
     public Asset? Asset { get; set; } = null;
 }
---
+++ server/BudgetBoard.Service/Models/Value.cs
@@ -0,0 +1,58 @@
+using BudgetBoard.Database.Models;
+
+namespace BudgetBoard.Service.Models;
+
+public interface IValueCreateRequest
+{
+    decimal Amount { get; set; }
+    DateTime DateTime { get; set; }
+    Guid AssetID { get; set; }
+}
+
+public class ValueCreateRequest() : IValueCreateRequest
+{
+    public decimal Amount { get; set; } = 0;
+    public DateTime DateTime { get; set; } = DateTime.UtcNow;
+    public Guid AssetID { get; set; } = Guid.Empty;
+}
+
+public interface IValueResponse
+{
+    Guid ID { get; set; }
+    decimal Amount { get; set; }
+    DateTime DateTime { get; set; }
+    DateTime? Deleted { get; set; }
+    Guid AssetID { get; set; }
+}
+
+public class ValueResponse() : IValueResponse
+{
+    public Guid ID { get; set; } = Guid.Empty;
+    public decimal Amount { get; set; } = 0;
+    public DateTime DateTime { get; set; } = DateTime.UtcNow;
+    public DateTime? Deleted { get; set; } = null;
+    public Guid AssetID { get; set; } = Guid.Empty;
+
+    public ValueResponse(Value value)
+        : this()
+    {
+        ID = value.ID;
+        Amount = value.Amount;
+        DateTime = value.DateTime;
+        AssetID = value.AssetID;
+    }
+}
+
+public interface IValueUpdateRequest
+{
+    Guid ID { get; set; }
+    decimal Amount { get; set; }
+    DateTime DateTime { get; set; }
+}
+
+public class ValueUpdateRequest() : IValueUpdateRequest
+{
+    public Guid ID { get; set; } = Guid.Empty;
+    public decimal Amount { get; set; } = 0;
+    public DateTime DateTime { get; set; } = DateTime.UtcNow;
+}
