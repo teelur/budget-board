b67c75f2dab58dd2af2d34048b86c8e4b1937992
teelur <tylershea12@gmail.com>
2025-11-05 22:43:03 -0500

    add value api

---
+++ server/BudgetBoard.WebAPI/Controllers/ValueController.cs
@@ -0,0 +1,160 @@
+using BudgetBoard.Database.Models;
+using BudgetBoard.Service.Interfaces;
+using BudgetBoard.Service.Models;
+using BudgetBoard.WebAPI.Utils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetBoard.WebAPI.Controllers;
+
+public class ValueController(
+    ILogger<ValueController> logger,
+    UserManager<ApplicationUser> userManager,
+    IValueService valueService
+) : ControllerBase
+{
+    private readonly ILogger<ValueController> _logger = logger;
+    private readonly UserManager<ApplicationUser> _userManager = userManager;
+    private readonly IValueService _valueService = valueService;
+
+    [HttpPost]
+    [Authorize]
+    public async Task<IActionResult> Create([FromBody] ValueCreateRequest value)
+    {
+        try
+        {
+            await _valueService.CreateValueAsync(
+                new Guid(_userManager.GetUserId(User) ?? string.Empty),
+                value
+            );
+            return Ok();
+        }
+        catch (BudgetBoardServiceException bbex)
+        {
+            return Helpers.BuildErrorResponse(bbex.Message);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An unexpected error occurred.");
+            return Helpers.BuildErrorResponse("An unexpected server error occurred.");
+        }
+    }
+
+    [HttpGet]
+    [Authorize]
+    public async Task<IActionResult> Read()
+    {
+        try
+        {
+            return Ok(
+                await _valueService.ReadValuesAsync(
+                    new Guid(_userManager.GetUserId(User) ?? string.Empty)
+                )
+            );
+        }
+        catch (BudgetBoardServiceException bbex)
+        {
+            return Helpers.BuildErrorResponse(bbex.Message);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An unexpected error occurred.");
+            return Helpers.BuildErrorResponse("An unexpected server error occurred.");
+        }
+    }
+
+    [HttpGet("{guid}")]
+    [Authorize]
+    public async Task<IActionResult> Read(Guid guid)
+    {
+        try
+        {
+            return Ok(
+                await _valueService.ReadValuesAsync(
+                    new Guid(_userManager.GetUserId(User) ?? string.Empty),
+                    guid
+                )
+            );
+        }
+        catch (BudgetBoardServiceException bbex)
+        {
+            return Helpers.BuildErrorResponse(bbex.Message);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An unexpected error occurred.");
+            return Helpers.BuildErrorResponse("An unexpected server error occurred.");
+        }
+    }
+
+    [HttpPut]
+    [Authorize]
+    public async Task<IActionResult> Update([FromBody] ValueUpdateRequest value)
+    {
+        try
+        {
+            await _valueService.UpdateValueAsync(
+                new Guid(_userManager.GetUserId(User) ?? string.Empty),
+                value
+            );
+            return Ok();
+        }
+        catch (BudgetBoardServiceException bbex)
+        {
+            return Helpers.BuildErrorResponse(bbex.Message);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An unexpected error occurred.");
+            return Helpers.BuildErrorResponse("An unexpected server error occurred.");
+        }
+    }
+
+    [HttpDelete]
+    [Authorize]
+    public async Task<IActionResult> Delete(Guid guid)
+    {
+        try
+        {
+            await _valueService.DeleteValueAsync(
+                new Guid(_userManager.GetUserId(User) ?? string.Empty),
+                guid
+            );
+            return Ok();
+        }
+        catch (BudgetBoardServiceException bbex)
+        {
+            return Helpers.BuildErrorResponse(bbex.Message);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An unexpected error occurred.");
+            return Helpers.BuildErrorResponse("An unexpected server error occurred.");
+        }
+    }
+
+    [HttpPost]
+    [Authorize]
+    [Route("[action]")]
+    public async Task<IActionResult> Restore(Guid guid)
+    {
+        try
+        {
+            await _valueService.RestoreValueAsync(
+                new Guid(_userManager.GetUserId(User) ?? string.Empty),
+                guid
+            );
+            return Ok();
+        }
+        catch (BudgetBoardServiceException bbex)
+        {
+            return Helpers.BuildErrorResponse(bbex.Message);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An unexpected error occurred.");
+            return Helpers.BuildErrorResponse("An unexpected server error occurred.");
+        }
+    }
+}
