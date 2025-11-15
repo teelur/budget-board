39a0a984d60ac919945a78530d13fa1e89c8f308
tshea <Tylershea12@gmail.com>
2025-11-15 14:12:37 -0600

    Fix replacement script

--- client/.docker/nginx/conf/nginx-substitute.sh
+++ client/.docker/nginx/conf/nginx-substitute.sh
@@ -2,4 +2,11 @@
 # Make sure your end of line is LF. Otherwise this will cause issues.
 export DOLLAR="$"
 envsubst < default.conf.template > /etc/nginx/conf.d/default.conf
+
+# Replace environment variables in projectEnvVariables.js
+projectEnvVariables=$(ls -t /usr/share/nginx/html/assets/projectEnvVariables*.js | head -n1)
+envsubst < "$projectEnvVariables" > /tmp/projectEnvVariables_temp
+cp /tmp/projectEnvVariables_temp "$projectEnvVariables"
+rm /tmp/projectEnvVariables_temp
+
 nginx -g "daemon off;"
