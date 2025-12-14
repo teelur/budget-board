a1c4fdd2f449b21e0435047403c2d342a386cbb5
tshea <Tylershea12@gmail.com>
2025-12-14 15:51:59 -0600

    Update create transactions modal to reference merchant name

--- client/src/app/Authorized/PageContent/Transactions/TransactionsHeader/CreateTransactionModal/CreateTransactionModal.tsx
+++ client/src/app/Authorized/PageContent/Transactions/TransactionsHeader/CreateTransactionModal/CreateTransactionModal.tsx
@@ -1,5 +1,5 @@
 import { ActionIcon, Button, Stack } from "@mantine/core";
-import { isNotEmpty, useForm } from "@mantine/form";
+import { useField } from "@mantine/form";
 import { useDisclosure } from "@mantine/hooks";
 import { notifications } from "@mantine/notifications";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
@@ -22,32 +22,23 @@ import NumberInput from "~/components/core/Input/NumberInput/NumberInput";
 import AccountSelect from "~/components/core/Select/AccountSelect/AccountSelect";
 import DateInput from "~/components/core/Input/DateInput/DateInput";
 
-interface formValues {
-  date: Date | null;
-  description: string;
-  category: string;
-  amount: number | string;
-  accountIds: string[];
-}
-
 const CreateTransactionModal = (): React.ReactNode => {
   const [opened, { open, close }] = useDisclosure(false);
 
-  const form = useForm<formValues>({
-    mode: "uncontrolled",
-    initialValues: {
-      date: new Date(),
-      description: "",
-      category: "",
-      amount: 0,
-      accountIds: [],
-    },
-
-    validate: {
-      date: (value) => (value ? null : "Date is required"),
-      description: isNotEmpty("name is required"),
-      accountIds: isNotEmpty("Account is required"),
-    },
+  const dateField = useField<Date | null>({
+    initialValue: new Date(),
+  });
+  const merchantNameField = useField<string>({
+    initialValue: "",
+  });
+  const categoryField = useField<string>({
+    initialValue: "",
+  });
+  const amountField = useField<number | string>({
+    initialValue: 0,
+  });
+  const accountIdsField = useField<string[]>({
+    initialValue: [],
   });
 
   const { transactionCategories } = useTransactionCategories();
@@ -91,15 +82,18 @@ const CreateTransactionModal = (): React.ReactNode => {
     },
   });
 
-  const onSubmit = (values: formValues) => {
-    if (!values.date) {
+  const onSubmit = () => {
+    if (!dateField.getValue()) {
       notifications.show({
         message: "Date is required",
         color: "var(--button-color-destructive)",
       });
       return;
     }
-    if (!values.accountIds || values.accountIds.length === 0) {
+    if (
+      !accountIdsField.getValue() ||
+      accountIdsField.getValue().length === 0
+    ) {
       notifications.show({
         message: "Account is required",
         color: "var(--button-color-destructive)",
@@ -108,14 +102,21 @@ const CreateTransactionModal = (): React.ReactNode => {
     }
 
     doCreateTransaction.mutate({
-      date: values.date,
-      merchantName: values.description,
-      category: getParentCategory(values.category, transactionCategories),
-      subcategory: getIsParentCategory(values.category, transactionCategories)
+      date: dateField.getValue()!,
+      merchantName: merchantNameField.getValue(),
+      category: getParentCategory(
+        categoryField.getValue(),
+        transactionCategories
+      ),
+      subcategory: getIsParentCategory(
+        categoryField.getValue(),
+        transactionCategories
+      )
         ? null
-        : values.category,
-      amount: values.amount === "" ? 0 : (values.amount as number),
-      accountID: values.accountIds[0]!,
+        : categoryField.getValue(),
+      amount:
+        amountField.getValue() === "" ? 0 : (amountField.getValue() as number),
+      accountID: accountIdsField.getValue()[0]!,
       source: AccountSource.Manual,
       syncID: null,
     });
@@ -129,66 +130,51 @@ const CreateTransactionModal = (): React.ReactNode => {
       <Modal
         opened={opened}
         onClose={close}
-        title="Create Transaction"
-        styles={{
-          inner: {
-            left: "0",
-            right: "0",
-            padding: "0 !important",
-          },
-        }}
+        title={<PrimaryText>Create Transaction</PrimaryText>}
       >
-        <form onSubmit={form.onSubmit(onSubmit)}>
-          <Stack gap={5}>
-            <DateInput
-              label={<PrimaryText size="sm">Date</PrimaryText>}
-              placeholder="Pick date"
-              {...form.getInputProps("date")}
-              elevation={0}
-            />
-            <TextInput
-              label={<PrimaryText size="sm">Description</PrimaryText>}
-              placeholder="Enter amount"
-              {...form.getInputProps("description")}
-              elevation={0}
-            />
-            <CategorySelect
-              label={<PrimaryText size="sm">Category</PrimaryText>}
-              categories={transactionCategories}
-              value={form.getValues().category}
-              onChange={(val) => form.setFieldValue("category", val)}
-              key={form.key("category")}
-              withinPortal
-              elevation={0}
-            />
-            <NumberInput
-              label={<PrimaryText size="sm">Amount</PrimaryText>}
-              placeholder="Enter amount"
-              prefix={getCurrencySymbol(userSettingsQuery.data?.currency)}
-              decimalScale={2}
-              thousandSeparator=","
-              {...form.getInputProps("amount")}
-              elevation={0}
-            />
-            <AccountSelect
-              label={<PrimaryText size="sm">Account</PrimaryText>}
-              selectedAccountIds={form.getValues().accountIds}
-              setSelectedAccountIds={(val) =>
-                form.setFieldValue("accountIds", val)
-              }
-              key={form.key("accountIds")}
-              maxSelectedValues={1}
-              elevation={0}
-            />
-            <Button
-              type="submit"
-              mt={5}
-              loading={doCreateTransaction.isPending}
-            >
-              Submit
-            </Button>
-          </Stack>
-        </form>
+        <Stack gap="0.25rem">
+          <DateInput
+            label={<PrimaryText size="sm">Date</PrimaryText>}
+            placeholder="Pick date"
+            {...dateField.getInputProps()}
+            elevation={0}
+          />
+          <TextInput
+            label={<PrimaryText size="sm">Merchant Name</PrimaryText>}
+            placeholder="Enter merchant name"
+            {...merchantNameField.getInputProps()}
+            elevation={0}
+          />
+          <CategorySelect
+            label={<PrimaryText size="sm">Category</PrimaryText>}
+            categories={transactionCategories}
+            {...categoryField.getInputProps()}
+            withinPortal
+            elevation={0}
+          />
+          <NumberInput
+            label={<PrimaryText size="sm">Amount</PrimaryText>}
+            placeholder="Enter amount"
+            prefix={getCurrencySymbol(userSettingsQuery.data?.currency)}
+            decimalScale={2}
+            thousandSeparator=","
+            {...amountField.getInputProps()}
+            elevation={0}
+          />
+          <AccountSelect
+            label={<PrimaryText size="sm">Account</PrimaryText>}
+            {...accountIdsField.getInputProps()}
+            maxSelectedValues={1}
+            elevation={0}
+          />
+          <Button
+            mt="0.25rem"
+            onClick={onSubmit}
+            loading={doCreateTransaction.isPending}
+          >
+            Submit
+          </Button>
+        </Stack>
       </Modal>
     </>
   );
