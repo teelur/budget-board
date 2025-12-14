b601c9e9adb3daeb31f7a5c26b3ba4f3e7b0a816
tshea <Tylershea12@gmail.com>
2025-12-13 20:46:45 -0600

    Update controllers

--- server/BudgetBoard.WebAPI/Controllers/NetWorthWidgetGroupController.cs
+++ server/BudgetBoard.WebAPI/Controllers/NetWorthWidgetGroupController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Localization;
 
-namespace BudgetBoard.Controllers;
+namespace BudgetBoard.WebAPI.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
