6e86b596fa26b6ed3ea46a3ce45715b11376947a
tshea <Tylershea12@gmail.com>
2025-12-15 17:32:38 -0600

    setup i18n

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,14 @@
-import { StrictMode } from "react";
+import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "~/App";
 import "~/shared/dayjs.ts";
+import "~/i18n/config.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Suspense fallback={<div>Loading...</div>}>
+      <App />
+    </Suspense>
   </StrictMode>
 );
