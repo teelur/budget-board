9919df323ad159e75d9516ee2a42353edb847f32
Tyler Shea <Tylershea12@gmail.com>
2026-04-03 23:34:44 -0500

    Minor dashboard tweaks (#790)
    * change dashboard card style
    
    * make header and navbar bold
    
    * make sidebar scrollable
    
    * Fix content height
    
    * address feedback
    
    * last feedback

--- client/src/app/Authorized/PageContent/Dashboard/AccountsCard/AccountsCard.tsx
+++ client/src/app/Authorized/PageContent/Dashboard/AccountsCard/AccountsCard.tsx
@@ -1,4 +1,4 @@
-import { Skeleton, Stack } from "@mantine/core";
+import { Group, Skeleton, Stack } from "@mantine/core";
 import React from "react";
 import { useAuth } from "~/providers/AuthProvider/AuthProvider";
 import { useQuery } from "@tanstack/react-query";
@@ -6,10 +6,13 @@ import { AxiosResponse } from "axios";
 import { IInstitution } from "~/models/institution";
 import InstitutionItem from "./InstitutionItems/InstitutionItem";
 import { IAccountResponse } from "~/models/account";
-import Card from "~/components/core/Card/Card";
 import PrimaryText from "~/components/core/Text/PrimaryText/PrimaryText";
 import DimmedText from "~/components/core/Text/DimmedText/DimmedText";
 import { useTranslation } from "react-i18next";
+import SplitCard, {
+  BorderThickness,
+} from "~/components/ui/SplitCard/SplitCard";
+import { LandmarkIcon } from "lucide-react";
 
 const AccountsCard = (): React.ReactNode => {
   const { t } = useTranslation();
@@ -52,7 +55,7 @@ const AccountsCard = (): React.ReactNode => {
       (institutionQuery.data ?? [])
         .filter((i) => i.deleted === null)
         .sort((a, b) => a.index - b.index),
-    [institutionQuery.data]
+    [institutionQuery.data],
   );
 
   const sortedFilteredInstitutionsForDisplay = React.useMemo(
@@ -60,34 +63,40 @@ const AccountsCard = (): React.ReactNode => {
       sortedFilteredInstitutions.filter(
         (i) =>
           i.accounts.filter(
-            (a) => a.deleted === null && a.hideAccount === false
-          ).length > 0
+            (a) => a.deleted === null && a.hideAccount === false,
+          ).length > 0,
       ),
-    [sortedFilteredInstitutions]
+    [sortedFilteredInstitutions],
   );
 
   return (
-    <Card w="100%" elevation={1}>
-      <Stack gap="0.5rem">
-        <PrimaryText size="xl">{t("accounts")}</PrimaryText>
-        <Stack align="center" gap="0.5rem">
-          {institutionQuery.isPending || accountsQuery.isPending ? (
-            <Skeleton height={600} radius="lg" />
-          ) : (sortedFilteredInstitutionsForDisplay ?? []).length > 0 ? (
-            (sortedFilteredInstitutionsForDisplay ?? []).map(
-              (institution: IInstitution) => (
-                <InstitutionItem
-                  key={institution.id}
-                  institution={institution}
-                />
-              )
-            )
-          ) : (
-            <DimmedText size="sm">{t("no_accounts_found")}</DimmedText>
-          )}
-        </Stack>
+    <SplitCard
+      w="100%"
+      border={BorderThickness.Thick}
+      header={
+        <Group gap={"0.25rem"}>
+          <LandmarkIcon color="var(--base-color-text-dimmed)" />
+          <PrimaryText size="xl" lh={1}>
+            {t("accounts")}
+          </PrimaryText>
+        </Group>
+      }
+      elevation={1}
+    >
+      <Stack align="center" gap="0.5rem">
+        {institutionQuery.isPending || accountsQuery.isPending ? (
+          <Skeleton height={600} radius="lg" />
+        ) : (sortedFilteredInstitutionsForDisplay ?? []).length > 0 ? (
+          (sortedFilteredInstitutionsForDisplay ?? []).map(
+            (institution: IInstitution) => (
+              <InstitutionItem key={institution.id} institution={institution} />
+            ),
+          )
+        ) : (
+          <DimmedText size="sm">{t("no_accounts_found")}</DimmedText>
+        )}
       </Stack>
-    </Card>
+    </SplitCard>
   );
 };
 
---
+++ client/src/components/ui/SplitCard/SplitCard.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Card, { CardProps } from "~/components/core/Card/Card";
+import { Flex, Stack } from "@mantine/core";
+import Divider from "~/components/core/Divider/Divider";
+
+export enum BorderThickness {
+  Normal,
+  Thick,
+}
+
+interface SplitCardProps extends CardProps {
+  border?: BorderThickness;
+  header: React.ReactNode;
+  elevation?: number;
+  children: React.ReactNode;
+}
+
+const SplitCard = ({
+  border,
+  header,
+  elevation,
+  style,
+  children,
+  ...props
+}: SplitCardProps): React.ReactNode => {
+  return (
+    <Card
+      w="100%"
+      p={0}
+      {...props}
+      style={{
+        ...style,
+        borderWidth: border === BorderThickness.Thick ? "2px" : "1px",
+      }}
+      elevation={elevation}
+    >
+      <Stack gap={0}>
+        <Flex w="100%" p={"0.5rem"} direction="column" align="stretch">
+          {header}
+        </Flex>
+        <Divider
+          size={border === BorderThickness.Thick ? "sm" : "xs"}
+          elevation={elevation}
+        />
+        <Flex w="100%" p={"0.5rem"} direction="column" align="stretch">
+          {children}
+        </Flex>
+      </Stack>
+    </Card>
+  );
+};
+
+export default SplitCard;
