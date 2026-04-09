0d2233c371a64ea4b03ba2ec62f9be89b334cf79
teelur <tylershea12@gmail.com>
2026-04-08 23:27:27 -0500

    Logo navigates back to dashboard

--- client/src/app/Authorized/Header/Header.tsx
+++ client/src/app/Authorized/Header/Header.tsx
@@ -1,9 +1,10 @@
 import BudgetBoardLogo from "~/assets/budget-board-logo";
 import classes from "./Header.module.css";
 
-import { Burger, Group, useComputedColorScheme } from "@mantine/core";
+import { Box, Burger, Group, useComputedColorScheme } from "@mantine/core";
 import SyncButton from "./SyncButton/SyncButton";
 import { areStringsEqual } from "~/helpers/utils";
+import { useNavigate } from "react-router";
 
 interface HeaderProps {
   isNavbarOpen: boolean;
@@ -12,6 +13,7 @@ interface HeaderProps {
 
 const Header = (props: HeaderProps): React.ReactNode => {
   const computedColorScheme = useComputedColorScheme();
+  const navigate = useNavigate();
   return (
     <Group
       p="0.5rem"
@@ -28,10 +30,15 @@ const Header = (props: HeaderProps): React.ReactNode => {
           hiddenFrom="xs"
           size="md"
         />
-        <BudgetBoardLogo
-          height={40}
-          darkMode={areStringsEqual(computedColorScheme, "dark")}
-        />
+        <Box
+          onClick={() => navigate("/dashboard")}
+          style={{ cursor: "pointer", lineHeight: 0 }}
+        >
+          <BudgetBoardLogo
+            height={40}
+            darkMode={areStringsEqual(computedColorScheme, "dark")}
+          />
+        </Box>
       </Group>
       <Group justify="flex-end" flex="1 0 auto">
         <SyncButton />
