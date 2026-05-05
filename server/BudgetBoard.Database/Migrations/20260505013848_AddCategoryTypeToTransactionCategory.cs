e98baddd9a2af693f4b5bd40ec8a3df9aa5b56f0
teelur <tylershea12@gmail.com>
2026-05-04 20:55:43 -0500

    update types

---
+++ server/BudgetBoard.Database/Migrations/20260505013848_AddCategoryTypeToTransactionCategory.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BudgetBoard.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryTypeToTransactionCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CategoryType",
+                table: "TransactionCategory",
+                type: "text",
+                nullable: true
+            );
+
+            migrationBuilder.Sql(
+                "UPDATE \"TransactionCategory\" SET \"CategoryType\" = 'expense' WHERE \"CategoryType\" IS NULL;"
+            );
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CategoryType",
+                table: "TransactionCategory",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "CategoryType", table: "TransactionCategory");
+        }
+    }
+}
