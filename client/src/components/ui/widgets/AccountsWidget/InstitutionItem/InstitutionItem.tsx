ed3b58fb158ff6b9a72b9def45d4f5159dc65efb
teelur <tylershea12@gmail.com>
2026-05-24 16:08:39 -0500

    rename directory

--- client/src/components/ui/widgets/AccountsWidget/InstitutionItem/InstitutionItem.tsx
+++ client/src/components/ui/widgets/AccountsWidget/InstitutionItem/InstitutionItem.tsx
@@ -23,29 +23,26 @@ const InstitutionItem = (props: InstitutionItemProps): React.ReactNode => {
   ).sort((a, b) => a.index - b.index);
 
   return (
-    <Card p={0} w="100%" elevation={2}>
-      <Stack gap={0}>
-        <Stack px="0.5rem" py="0.25rem">
-          <PrimaryText className={classes.title}>
-            {props.institution.name}
-          </PrimaryText>
-        </Stack>
-        <Divider my="0.125rem" size="xs" elevation={2} />
-        <Stack px="0.5rem" py="0.5rem" gap="0.25rem">
-          {sortedFilteredAccounts.map((account: IAccountResponse) => (
-            <AccountItem
-              key={account.id}
-              account={account}
-              onClick={() => {
-                const filters = new Filters();
-                filters.accounts = [account.id];
-                navigateToTransactions(filters);
-              }}
-            />
-          ))}
-        </Stack>
+    <Stack gap={0}>
+      <Stack px="0.5rem" py="0.25rem">
+        <PrimaryText className={classes.title}>
+          {props.institution.name}
+        </PrimaryText>
       </Stack>
-    </Card>
+      <Stack px="0.5rem" py="0.5rem" gap="0.25rem">
+        {sortedFilteredAccounts.map((account: IAccountResponse) => (
+          <AccountItem
+            key={account.id}
+            account={account}
+            onClick={() => {
+              const filters = new Filters();
+              filters.accounts = [account.id];
+              navigateToTransactions(filters);
+            }}
+          />
+        ))}
+      </Stack>
+    </Stack>
   );
 };
 
