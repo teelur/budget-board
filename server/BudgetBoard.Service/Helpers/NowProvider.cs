d6730dd06c1a0e9d1263148a6d796ba540bdc13b
Tyler Shea <Tylershea12@gmail.com>
2026-06-16 23:30:12 -0500

    Fix now provider (#935)
    * Fix now provider
    
    * address feedback

--- server/BudgetBoard.Service/Helpers/NowProvider.cs
+++ server/BudgetBoard.Service/Helpers/NowProvider.cs
@@ -6,11 +6,8 @@ public interface INowProvider
     DateTime UtcNow { get; }
 }
 
-internal class NowProvider : INowProvider
+public class NowProvider : INowProvider
 {
-    private static readonly Lazy<NowProvider> _instance = new(() => new NowProvider());
-    public static NowProvider Instance => _instance.Value;
-
     public DateTime Now => DateTime.Now;
     public DateTime UtcNow => DateTime.UtcNow;
 }
