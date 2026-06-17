7916b9fb7c43da2f63e29337b581242ba928aa8e
teelur <tylershea12@gmail.com>
2026-06-16 23:26:45 -0500

    address feedback

--- server/BudgetBoard.Service/Helpers/NowProvider.cs
+++ server/BudgetBoard.Service/Helpers/NowProvider.cs
@@ -8,9 +8,6 @@ public interface INowProvider
 
 public class NowProvider : INowProvider
 {
-    private static readonly Lazy<NowProvider> _instance = new(() => new NowProvider());
-    public static NowProvider Instance => _instance.Value;
-
     public DateTime Now => DateTime.Now;
     public DateTime UtcNow => DateTime.UtcNow;
 }
