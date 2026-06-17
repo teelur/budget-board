dfe8f92a0390325598dc39e58267771532178ad0
teelur <tylershea12@gmail.com>
2026-06-17 18:44:32 -0500

    make asset type property not nullable

---
+++ server/BudgetBoard.Database/Migrations/20260617233757_MakeAssetTypeRequired.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BudgetBoard.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeAssetTypeRequired : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                UPDATE "Asset"
+                SET "Type" = ''
+                WHERE "Type" IS NULL;
+                """
+            );
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Asset",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Type",
+                table: "Asset",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text"
+            );
+        }
+    }
+}
