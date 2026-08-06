5130b9f1caed6b04fddc82d377e255efa40fcf30
teelur <tylershea12@gmail.com>
2026-08-05 19:46:07 -0500

    Automatic transaction categorizer test cleanup

--- server/BudgetBoard.Tests/Helpers/AutomaticTransactionCategorizerTests.cs
+++ server/BudgetBoard.Tests/Helpers/AutomaticTransactionCategorizerTests.cs
@@ -1,7 +1,9 @@
+using BudgetBoard.Database.Interfaces;
 using BudgetBoard.Database.Models;
 using BudgetBoard.Service.Helpers;
 using BudgetBoard.Service.Models;
 using FluentAssertions;
+using Moq;
 
 namespace BudgetBoard.IntegrationTests.Helpers;
 
@@ -143,5 +145,181 @@ public class AutomaticTransactionCategorizerTests
         // Assert
         category1.Should().Be("Category2");
         category2.Should().Be("Category5");
+    }
+
+    [Fact]
+    public void AutomaticTransactionCategorizer_WhenSubcategoryIsPresent_UsesSubcategoryAsTrainingLabel()
+    {
+        account.Transactions.First().Subcategory = "Subcategory1";
+        account.Transactions.Skip(1).First().Subcategory = string.Empty;
+        account.Transactions.Skip(2).First().MerchantName = null;
+        account.Transactions.Skip(3).First().Category = null;
+
+        var mlModel = AutomaticTransactionCategorizerHelper.Train(account.Transactions);
+
+        mlModel.Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public void CalculatePredictionProbability_WhenScoresAreNull_ReturnsZero()
+    {
+        var result = AutomaticTransactionCategorizerHelper.CalculatePredictionProbability(null);
+
+        result.Should().Be(0f);
+    }
+
+    [Fact]
+    public void CalculatePredictionProbability_WhenScoresAreEmpty_ReturnsZero()
+    {
+        var result = AutomaticTransactionCategorizerHelper.CalculatePredictionProbability([]);
+
+        result.Should().Be(0f);
+    }
+
+    [Fact]
+    public void CalculatePredictionProbability_WhenScoresArePresent_ReturnsHighestProbability()
+    {
+        var result = AutomaticTransactionCategorizerHelper.CalculatePredictionProbability(
+            [1f, 2f, 3f]
+        );
+
+        result.Should().BeApproximately(0.6652f, 0.0001f);
+    }
+
+    [Fact]
+    public void GetPredictionCategory_WhenLabelIsNull_ReturnsEmptyString()
+    {
+        var result = AutomaticTransactionCategorizerHelper.GetPredictionCategory(null);
+
+        result.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void GetPredictionCategory_WhenLabelIsPresent_ReturnsLabel()
+    {
+        var result = AutomaticTransactionCategorizerHelper.GetPredictionCategory("Category");
+
+        result.Should().Be("Category");
+    }
+
+    [Fact]
+    public async Task CreateAutoCategorizerAsync_WhenUserSettingsAreMissing_ReturnsNull()
+    {
+        var user = new ApplicationUser { Id = account.UserID };
+        var store = new Mock<ILargeObjectStore>();
+
+        var result = await AutomaticTransactionCategorizerHelper.CreateAutoCategorizerAsync(
+            store.Object,
+            user
+        );
+
+        result.Should().BeNull();
+        store.Verify(
+            largeObjectStore => largeObjectStore.ReadLargeObjectAsync(It.IsAny<long>()),
+            Times.Never
+        );
+    }
+
+    [Fact]
+    public async Task CreateAutoCategorizerAsync_WhenAutoCategorizerIsDisabled_ReturnsNull()
+    {
+        var user = CreateUser(enabled: false);
+        var store = new Mock<ILargeObjectStore>();
+
+        var result = await AutomaticTransactionCategorizerHelper.CreateAutoCategorizerAsync(
+            store.Object,
+            user
+        );
+
+        result.Should().BeNull();
+        store.Verify(
+            largeObjectStore => largeObjectStore.ReadLargeObjectAsync(It.IsAny<long>()),
+            Times.Never
+        );
+    }
+
+    [Fact]
+    public async Task CreateAutoCategorizerAsync_WhenModelOidIsMissing_ReturnsNull()
+    {
+        var user = CreateUser(modelObjectId: null);
+        var store = new Mock<ILargeObjectStore>();
+
+        var result = await AutomaticTransactionCategorizerHelper.CreateAutoCategorizerAsync(
+            store.Object,
+            user
+        );
+
+        result.Should().BeNull();
+        store.Verify(
+            largeObjectStore => largeObjectStore.ReadLargeObjectAsync(It.IsAny<long>()),
+            Times.Never
+        );
+    }
+
+    [Fact]
+    public async Task CreateAutoCategorizerAsync_WhenModelBytesAreNull_ReturnsNull()
+    {
+        var user = CreateUser();
+        var store = new Mock<ILargeObjectStore>();
+        store
+            .Setup(largeObjectStore => largeObjectStore.ReadLargeObjectAsync(123))
+            .ReturnsAsync((byte[]?)null);
+
+        var result = await AutomaticTransactionCategorizerHelper.CreateAutoCategorizerAsync(
+            store.Object,
+            user
+        );
+
+        result.Should().BeNull();
+        store.Verify(largeObjectStore => largeObjectStore.ReadLargeObjectAsync(123), Times.Once);
+    }
+
+    [Fact]
+    public async Task CreateAutoCategorizerAsync_WhenModelBytesAreEmpty_ReturnsNull()
+    {
+        var user = CreateUser();
+        var store = new Mock<ILargeObjectStore>();
+        store
+            .Setup(largeObjectStore => largeObjectStore.ReadLargeObjectAsync(123))
+            .ReturnsAsync([]);
+
+        var result = await AutomaticTransactionCategorizerHelper.CreateAutoCategorizerAsync(
+            store.Object,
+            user
+        );
+
+        result.Should().BeNull();
+        store.Verify(largeObjectStore => largeObjectStore.ReadLargeObjectAsync(123), Times.Once);
+    }
+
+    [Fact]
+    public async Task CreateAutoCategorizerAsync_WhenModelBytesAreValid_ReturnsCategorizer()
+    {
+        var user = CreateUser();
+        var store = new Mock<ILargeObjectStore>();
+        var mlModel = AutomaticTransactionCategorizerHelper.Train(account.Transactions);
+        store
+            .Setup(largeObjectStore => largeObjectStore.ReadLargeObjectAsync(123))
+            .ReturnsAsync(mlModel);
+
+        var result = await AutomaticTransactionCategorizerHelper.CreateAutoCategorizerAsync(
+            store.Object,
+            user
+        );
+
+        result.Should().NotBeNull();
+        store.Verify(largeObjectStore => largeObjectStore.ReadLargeObjectAsync(123), Times.Once);
+    }
+
+    private ApplicationUser CreateUser(bool enabled = true, long? modelObjectId = 123)
+    {
+        var user = new ApplicationUser { Id = account.UserID };
+        user.UserSettings = new UserSettings
+        {
+            UserID = user.Id,
+            EnableAutoCategorizer = enabled,
+            AutoCategorizerModelOID = modelObjectId,
+        };
+        return user;
     }
 }
