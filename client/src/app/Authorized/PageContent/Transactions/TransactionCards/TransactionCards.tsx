e695b1a0ce9a2603b723c9397b2a5b99959dfbed
teelur <tylershea12@gmail.com>
2026-08-15 13:53:55 -0500

    Update transactions cards to virtualized list

--- client/src/app/Authorized/PageContent/Transactions/TransactionCards/TransactionCards.tsx
+++ client/src/app/Authorized/PageContent/Transactions/TransactionCards/TransactionCards.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useVirtualizer } from "@tanstack/react-virtual";
 import { ITransaction } from "~/models/transaction";
 import { Group, Skeleton, Stack } from "@mantine/core";
 import { useTransactionCategories } from "~/providers/TransactionCategoryProvider/TransactionCategoryProvider";
@@ -6,6 +7,7 @@ import { useTranslation } from "react-i18next";
 import TransactionCard from "~/components/core/Card/TransactionCard/TransactionCard";
 import DimmedText from "~/components/core/Text/DimmedText/DimmedText";
 import { InfoIcon } from "lucide-react";
+import classes from "./TransactionCards.module.css";
 
 interface TransactionCardsProps {
   currentViewTransactions: ITransaction[];
@@ -19,33 +21,62 @@ const TransactionCards = (props: TransactionCardsProps): React.ReactNode => {
 
   const { t } = useTranslation();
   const { allTransactionCategories } = useTransactionCategories();
+  const viewportRef = React.useRef<HTMLDivElement>(null);
+  const virtualizer = useVirtualizer({
+    count: props.currentViewTransactions.length,
+    getScrollElement: () => viewportRef.current,
+    estimateSize: () => 56,
+    getItemKey: (index) => props.currentViewTransactions[index]?.id ?? index,
+    overscan: 5,
+  });
 
   return (
-    <Stack gap="0.5rem">
+    <Stack gap="0.5rem" className={classes.container}>
       {props.isQueryPending ? (
         Array.from({ length: skeletonCount }).map((_, index) => (
           <Skeleton key={index} height={40} radius="md" />
         ))
       ) : (
-        <Stack gap="0.3rem" align="center">
+        <div ref={viewportRef} className={classes.viewport}>
           {props.currentViewTransactions.length > 0 ? (
-            props.currentViewTransactions.map((transaction) => (
-              <TransactionCard
-                key={transaction.id}
-                transaction={transaction}
-                categories={allTransactionCategories}
-                elevation={1}
-                isSelected={props.selectedIds.has(transaction.id)}
-                onToggleSelect={props.onToggleSelect}
-              />
-            ))
+            <div
+              className={classes.virtualList}
+              style={{ height: virtualizer.getTotalSize() }}
+            >
+              {virtualizer.getVirtualItems().map((virtualItem) => {
+                const transaction =
+                  props.currentViewTransactions[virtualItem.index];
+
+                if (transaction === undefined) {
+                  return null;
+                }
+
+                return (
+                  <div
+                    key={virtualItem.key}
+                    data-index={virtualItem.index}
+                    ref={virtualizer.measureElement}
+                    className={classes.virtualRow}
+                    style={{ transform: `translateY(${virtualItem.start}px)` }}
+                  >
+                    <TransactionCard
+                      transaction={transaction}
+                      categories={allTransactionCategories}
+                      elevation={1}
+                      isSelected={props.selectedIds.has(transaction.id)}
+                      onToggleSelect={props.onToggleSelect}
+                    />
+                  </div>
+                );
+              })}
+            </div>
           ) : (
             <Group justify="center" align="center" gap="0.5rem">
               <InfoIcon size={20} color="var(--base-color-text-dimmed)" />
               <DimmedText size="sm">{t("no_transactions")}</DimmedText>
             </Group>
           )}
-        </Stack>
+        </div>
       )}
     </Stack>
   );
